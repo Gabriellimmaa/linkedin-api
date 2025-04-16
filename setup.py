754f3981782eaa7ac2824711769c5e7e335e71b9
Gabriel Lima de Moraes <gabriellimamoraes@gmail.com>
2025-04-16 04:22:54 -0300

    2.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="linkedin_api",
-    version="2.0.1", 
+    version="2.0.2", 
     packages=find_packages(),
     install_requires=[
         "requests>=2.22.0",
